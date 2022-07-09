b001e5465839a59864582f8a3cc1441c2fc16930
Yenyu92 <82609525+Yenyu92@users.noreply.github.com>
2022-07-09 17:06:29 -0500

    Add files via upload

---
+++ open-bootcamp-suma/src/open/bootcamp/Main.java
@@ -0,0 +1,15 @@
+package open.bootcamp;
+
+public class Main {
+
+    public static void main(String[] args) {
+       suma(10,120,30);
+    }
+    
+    public static void suma (int a, int b, int c){
+        int resultado;
+        resultado = a + b + c;
+        System.out.println(resultado);   
+    }
+}
+
